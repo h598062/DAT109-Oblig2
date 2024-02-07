fa9704c532921558bb0e5c8326b8247d4e6a6fa4
h598062 <598062@stud.hvl.no>
2024-02-07 11:03:35 +0100

    lagt til kunde

---
+++ src/main/java/no/hvl/dat109/Kunde.java
@@ -0,0 +1,15 @@
+package no.hvl.dat109;
+
+public class Kunde {
+	private String fornavn;
+	private String etternavn;
+	private String adresse;
+	private String telefonnummer;
+
+	public Kunde(String fornavn, String etternavn, String adresse, String telefonnummer) {
+		this.fornavn       = fornavn;
+		this.etternavn     = etternavn;
+		this.adresse       = adresse;
+		this.telefonnummer = telefonnummer;
+	}
+}
