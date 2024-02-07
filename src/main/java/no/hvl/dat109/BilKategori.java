3284c25479a05a1cfab1cfee50dc3d229f07ca1c
h598062 <598062@stud.hvl.no>
2024-02-07 15:10:14 +0100

    lagt in priser for BilKategori

--- src/main/java/no/hvl/dat109/BilKategori.java
+++ src/main/java/no/hvl/dat109/BilKategori.java
@@ -1,5 +1,14 @@
 package no.hvl.dat109;
 
 public enum BilKategori {
-	A, B, C, D;
+	A(800), B(1200), C(1800), D(2500);
+
+	private final int pris;
+	BilKategori(int pris) {
+		this.pris = pris;
+	}
+
+	public int pris() {
+		return pris;
+	}
 }
